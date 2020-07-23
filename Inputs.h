4922efba7ab7693ebf488b564b345d1af16d76ee
yinweijie <ywj123450@gmail.com>
2020-07-23 19:57:31 +0800

    Inputs.h, Mesh.h

---
+++ Inputs.h
@@ -0,0 +1,28 @@
+#ifndef INPUTS_H
+#define INPUTS_H
+
+namespace Inputs
+{
+    // # Thermal Conductivity of the bar (W/mK)
+    constexpr double k = 100;
+    
+    // # Cross-sectional Area of the bar (m2)
+    constexpr double A = 0.1;
+    
+    // # Length of the bar (m)
+    constexpr double L = 5;
+    
+    // # Number of cells in the mesh
+    constexpr int N = 5;
+    
+    // # Temperature at the left hand side of the bar (deg C)
+    constexpr double T_A = 100;
+    
+    // # Temperature at the left hand side of the bar (deg C)
+    constexpr double T_B = 200;
+    
+    // # Heat source per unit volume (W/m3)
+    constexpr double S_bar = 1000;
+}
+
+#endif
